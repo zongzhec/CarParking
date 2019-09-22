b89abc0453e302cc27d24c0d6967c7ed255626bc
zongzhec <zongzhe_chen@sina.com>
2019-09-22 18:22:39 +0800

    Implement Main Functions

--- src/main/java/foo/zongzhe/carpark/builder/CarParking.java
+++ src/main/java/foo/zongzhe/carpark/builder/CarParking.java
@@ -6,6 +6,7 @@ import foo.zongzhe.common.utils.LogUtil;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,43 +15,83 @@ import java.util.List;
 public class CarParking {
 
     // Main logic entry
+    final static int MON_PRICE = 750;
+    final static int MONTH = 12;
 
 
     public static void main(String args[]) {
 
+        // Input validation
+        String startDate = (args.length == 0) ? "2019-01-02" : args[0];
+        LogUtil.logInfo(String.format("Start parking date is %s", startDate));
+
+        // Initialize
         List<Calendar> holidayList = addHolidayCode();
         LogUtil.logInfo("Recorded holidays amount: " + holidayList.size());
 
-        for (Calendar c:holidayList){
-            LogUtil.logInfo(c.);
+        LogUtil.logInfo("holiday list: ");
+        for (Calendar c : holidayList) {
+            LogUtil.logInfo(DateUtil.dateToString(c.getTime(), DateUtil.YYYY_MM_DD_WITH_HYPHEN));
         }
 
 
-        int priceMon = 750;
-        int[] parkUse = new int[12];
-        String[] parkStartDate = new String[12];
-        String[] parkEndDate = new String[12];
+//        int[] parkDays = new int[12];
+//        Calendar[] parkStartDate = new Calendar[12];
+//        Calendar[] parkEndDate = new Calendar[12];
 
-        for (int i : parkUse) {
-            parkUse[i] = 0;
-            parkStartDate[i] = "";
-            parkEndDate[i] = "";
+        Calendar startDateC = Calendar.getInstance();
+        startDateC = DateUtil.stringToCalendar(startDate, DateUtil.YYYY_MM_DD_WITH_HYPHEN);
+
+//        for (int i : parkDays) {
+//            parkDays[i] = 0;
+//            parkStartDate[i] = Calendar.getInstance();
+//            parkEndDate[i] = Calendar.getInstance();
+//        }
+//
+//        parkStartDate[0] = DateUtil.stringToCalendar(startDate, DateUtil.YYYY_MM_DD_WITH_HYPHEN);
+        for (int i = 1; i <= MONTH; i++) {
+            LogUtil.logInfo(String.format("Analyzing month: %d, and the start date is %s",
+                    i, DateUtil.dateToString(startDateC.getTime(), DateUtil.YYYY_MM_DD_WITH_HYPHEN)));
+            startDateC = getParkDays(startDateC, holidayList);
         }
 
-        DateFormat df = new SimpleDateFormat(DateUtil.YYYY_MM_DD_WITH_HYPHEN);
-        Date date = null;
-        try {
-            date = df.parse("2019-01-01");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Calendar currentDate = Calendar.getInstance();
-        Calendar tempDate = Calendar.getInstance();
-        currentDate.setTime(date);
-        tempDate.setTime(date);
-        holidayList.add(tempDate);
-        LogUtil.logInfo(String.valueOf(holidayList.contains(currentDate)));
+        // Main functions
+//        parkEndDate[0] = DateUtil.CopyCalendar(parkStartDate[0]);
+//        parkStartDate[1] = getParkDays(parkStartDate[0], holidayList);
+//
+//        DateFormat df = new SimpleDateFormat(DateUtil.YYYY_MM_DD_WITH_HYPHEN);
+//        Date date = null;
+//        try {
+//            date = df.parse("2019-01-01");
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        Calendar currentDate = Calendar.getInstance();
+//        Calendar tempDate = Calendar.getInstance();
+//        currentDate.setTime(date);
+//        tempDate.setTime(date);
+//        holidayList.add(tempDate);
+//        LogUtil.logInfo(String.valueOf(holidayList.contains(currentDate)));
 
+    }
+
+    static Calendar getParkDays(Calendar startCalendar, List<Calendar> holidayList) {
+        Calendar endCalendar = DateUtil.CopyCalendar(startCalendar);
+        endCalendar.add(Calendar.MONTH, 1);
+        endCalendar.add(Calendar.DAY_OF_MONTH, -1);
+        if (endCalendar.get(Calendar.YEAR) == startCalendar.get(Calendar.YEAR) + 1) {
+            endCalendar.set(Calendar.YEAR, startCalendar.get(Calendar.YEAR));
+            endCalendar.set(Calendar.MONTH, 12);
+            endCalendar.set(Calendar.DAY_OF_MONTH, 31);
+        }
+        LogUtil.logInfo(String.format("Start date: %s, end date: %s",
+                DateUtil.dateToString(startCalendar.getTime(), DateUtil.YYYY_MM_DD_WITH_HYPHEN),
+                DateUtil.dateToString(endCalendar.getTime(), DateUtil.YYYY_MM_DD_WITH_HYPHEN)));
+        int days = DateUtil.getBusDaysWithinPeriod(startCalendar, endCalendar, holidayList);
+        Double dailyPrice = (double) (MON_PRICE / days);
+        LogUtil.logInfo(String.format("Parking for %d days, and costing %.0f RMB each day", days, dailyPrice));
+        endCalendar = DateUtil.getNextBusDate(endCalendar, holidayList);
+        return endCalendar;
     }
 
     public static List<Calendar> addHolidayCode() {
@@ -72,7 +113,7 @@ public class CarParking {
         holidayCode.add("2019-10-03");
         holidayCode.add("2019-10-04");
         holidayCode.add("2019-10-07");
-        for (String holiday:holidayCode){
+        for (String holiday : holidayCode) {
             DateFormat df = new SimpleDateFormat(DateUtil.YYYY_MM_DD_WITH_HYPHEN);
             Date date = new Date();
             try {
